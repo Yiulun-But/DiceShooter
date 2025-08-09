90029274540178f5a094087c742bab41a08dcc54
slothagami <slothagami@gmail.com>
2025-08-10 01:29:12 +1000

    moved level data to their own files and chose them in inspector

--- rythumdice/scripts/level_generator.gd
+++ rythumdice/scripts/level_generator.gd
@@ -1,25 +1,12 @@
 extends Node3D
 
-const level = {
-	"bpm": 120,
-	"audio": "res://audio/basic_song.wav",
-	"dice": [
-		1, 1, 1, 1, 5, 5, 5, 5, 
-		5, 5, 5, 5, 5, 5, 5, 5,
-		5, 2, 5, 2, 5, 2, 5, 2,
-		5, 2, 5, 2, 5, 2, 5, 2,
-		6, 2, 6, 2, 6, 2, 6, 2,
-		6, 2, 6, 2, 6, 2, 6, 2,
-		6, 2, 6, 2, 6, 2, 6, 2,
-		6, 2, 6, 2, 6, 2, 6, 2,
-	]
-}
-
-const die_spacing = 1.2
+@export_file("*.json") var level
 @onready var dice = preload("res://scenes/die.tscn")
 
 var bgm
+var level_data
 var target_pos = position
+const die_spacing = 1.2
 
 func timings_from_beatmap(beatmap):
 	var timings = []
@@ -33,15 +20,17 @@ func timings_from_beatmap(beatmap):
 func _ready():
 	add_to_group("levelgen")
 	
+	level_data = load_json(level)
+	
 	# setup bgm node to handle note timings
 	bgm = get_tree().get_nodes_in_group("bgm")[0]
-	bgm.tempo = level.bpm
-	bgm.audio = level.audio
+	bgm.tempo = level_data.bpm
+	bgm.audio = level_data.audio
 	bgm.start_song()
 	
 	# generate dice sequence
 	var prev_die = null
-	var dice_sequence = level.dice
+	var dice_sequence = level_data.dice
 	for i in range(dice_sequence.size()):
 		var die = dice.instantiate()
 		
@@ -58,3 +47,11 @@ func _ready():
 
 func _process(_delta):
 	position = lerp(position, target_pos, .1)
+	
+func load_json(file_path):
+	var file = FileAccess.open(file_path, FileAccess.READ)
+	if file == null: push_error("Could not open level file: \"", file_path, '"')
+	
+	var data = JSON.parse_string(file.get_as_text())
+	if data == null: push_error("Invalid JSON in file: \"", file_path, '"')
+	return data
