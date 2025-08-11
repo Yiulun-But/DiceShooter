40e27b071fcd51ceaa33ace5fb4fd0a3930feb33
Shangren <zhoushangren@outlook.com>
2025-08-11 22:21:30 +1000

    changed score to a constant

--- rythumdice/scripts/score.gd
+++ rythumdice/scripts/score.gd
@@ -4,10 +4,10 @@ var bgm
 var level_data
  
 const SCORING_FACING = 6
+const SCORE_PER_BEAT = 60
+const SCORE_PER_BEAT_BONUS = 20
 # score
 var score
-var score_per_beat
-
 @onready var label: Label3D = $Label3D
 
 func _ready() -> void:
@@ -22,16 +22,13 @@ func _ready() -> void:
 	for e in get_tree().get_nodes_in_group("dices"):
 		e.dice_finished.connect(_on_dice_finished)
 	
-	# initialize score data
-	score_per_beat = 100. / bgm.timings.size()
-	
 	# initialize score display
-	label.text = "0 %"
+	label.text = "0"
 		
 		
 
 func _process(_delta: float) -> void:
-	label.text = "%0.2f" % score + " %"
+	label.text = "%0.2f" % score
 	
 func _on_dice_finished(moves: int, facing: int):
 	if facing == SCORING_FACING:
@@ -39,6 +36,6 @@ func _on_dice_finished(moves: int, facing: int):
 	#print("moves: ", moves, ", facing: ", facing)
 
 func add_score(moves: int):
-	score += score_per_beat
+	score += SCORE_PER_BEAT
 	if (moves <= 1):
-		score += score_per_beat * 0.01
+		score += SCORE_PER_BEAT_BONUS
