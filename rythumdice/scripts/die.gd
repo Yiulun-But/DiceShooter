74668c10fa3d4e5d4aec146aef1f86f9e5ced2d0
slothagami <slothagami@gmail.com>
2025-08-12 15:25:20 +1000

    change to using input event to avoid dropped inputs

--- rythumdice/scripts/die.gd
+++ rythumdice/scripts/die.gd
@@ -87,12 +87,15 @@ func _process(_delta):
 	scale = Vector3.ONE
 	if active:
 		scale = Vector3.ONE * 1.1
-		
+
+func _input(event):
+	if event is not InputEventKey: return
+	if active and not event.echo:
 		var camera_basis = camera.global_transform.basis
-		if Input.is_action_just_pressed("up"   ): rot(camera_basis.x, -rot_angle)
-		if Input.is_action_just_pressed("down" ): rot(camera_basis.x,  rot_angle)
-		if Input.is_action_just_pressed("right"): rot(camera_basis.y,  rot_angle)
-		if Input.is_action_just_pressed("left" ): rot(camera_basis.y, -rot_angle)
+		if event.is_action_pressed("up"   ): rot(camera_basis.x, -rot_angle)
+		if event.is_action_pressed("down" ): rot(camera_basis.x,  rot_angle)
+		if event.is_action_pressed("right"): rot(camera_basis.y,  rot_angle)
+		if event.is_action_pressed("left" ): rot(camera_basis.y, -rot_angle)
 		moves += 1
 
 func _on_beat():
