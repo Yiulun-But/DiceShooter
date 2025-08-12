9fefc344a027093df44f75567a9a44bd721c8a5f
Yiulun-But <115389719+Yiulun-But@users.noreply.github.com>
2025-08-12 20:57:54 +1000

    Adjust bpm of level 1

--- rythumdice/scripts/menu_control.gd
+++ rythumdice/scripts/menu_control.gd
@@ -1,7 +1,7 @@
 extends CanvasLayer
 
 func _on_button1_pressed():
-	global.selected_level = "res://levels/tetris.json"
+	global.selected_level = "res://levels/level1.json"
 	get_tree().change_scene_to_file("res://scenes/game.tscn")
 
 func _on_button2_pressed():
