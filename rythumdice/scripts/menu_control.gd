7f4462cef10754ad6a60277311803f33ca82ffc9
slothagami <slothagami@gmail.com>
2025-08-13 01:54:12 +1000

    finish game

--- rythumdice/scripts/menu_control.gd
+++ rythumdice/scripts/menu_control.gd
@@ -5,7 +5,7 @@ func _on_button1_pressed():
 	get_tree().change_scene_to_file("res://scenes/game.tscn")
 
 func _on_button2_pressed():
-	global.selected_level = "res://levels/level3.json"
+	global.selected_level = "res://levels/level2.json"
 	get_tree().change_scene_to_file("res://scenes/game.tscn")
 
 func _on_button3_pressed():
